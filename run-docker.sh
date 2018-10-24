7ce35ff57e0951d0de346d0db3da5a2e4bd380fd
bsantosh <bhiravabhatla@gmail.com>
2018-10-24 09:56:29 +0530

    @Santosh : Updates run-docker.sh to use environment variables from user.properties file

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "The env is $1 $2 - in run-docker.sh"
-docker run -v ~/.m2:/root/.m2:rw -v ~/projects/bahmni/bahmni-gauge:/gauge -e ENV=$1 -e TAGS=$2 -e BAHMNI_GAUGE_APP_LOCATION=Registration -e BAHMNI_GAUGE_APP_URL=https://qa-reporting.ehealthunit.org -e BAHMNI_GAUGE_APP_PASSWORD=P@ssw0rd -e BAHMNI_GAUGE_APP_LOCALE=en -e BAHMNI_GAUGE_APP_USER=superman -e RUNS_IN_DOCKER=true -i anallytics/docker-gauge-chromedriver:latest -- sh run.sh
+docker run -v ~/.m2:/root/.m2:rw -v ~/projects/bahmni/bahmni-gauge:/gauge -e ENV=$1 -e TAGS=$2 -i anallytics/docker-gauge-chromedriver:latest -- sh run.sh
 #Hack. The html-report executable is symlinked as a root user.  
 #so, removing it so that the artifact is accessible from gocd server
 rc=$?
