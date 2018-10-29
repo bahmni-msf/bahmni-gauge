ffebd520b8a0718f1b83b39ac79d7fa1c8c5d83b
bsantosh <bhiravabhatla@gmail.com>
2018-10-30 00:05:29 +0530

    @Santosh :Changes maven build command to build only required modules

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 nohup Xvfb -ac :99 -screen 0 1280x1024x16 &
 export DISPLAY=:99
-mvn clean install -DskipTests
+mvn -pl "bahmni-gauge-commons,bahmni-gauge-default" clean install -DskipTests
 echo "The env in run.sh $ENV $TAGS"
 cd bahmni-gauge-default/ && mvn gauge:execute -Denv=$ENV -Dtags="$TAGS"
