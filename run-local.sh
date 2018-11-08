fb060062150f748cd203b94d0cd38d086264fb6f
bsantosh <bhiravabhatla@gmail.com>
2018-11-08 17:24:56 +0530

    @santosh - Updates run-local.sh to run mvn clean install on all modules

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mvn -pl "bahmni-gauge-commons,bahmni-gauge-default" clean install -DskipTests
+mvn  clean install -DskipTests
 echo "The env in run.sh $1 $2"
 cd bahmni-gauge-default/ && mvn gauge:execute -Denv=$1 -Dtags="$2"
