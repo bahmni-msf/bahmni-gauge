e04bbc41cbff601792f88d2387de629a6c1f55e9
bsantosh <bhiravabhatla@gmail.com>
2019-04-08 15:34:46 +0530

    Santosh | #00 | Cleans up property files

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn -pl "bahmni-gauge-commons,bahmni-gauge-amman"  clean install -DskipTests
-echo "The env in run.sh $1 $2"
-cd bahmni-gauge-amman/ && mvn gauge:execute -Denv=$1 -Dtags="$2"
+echo "The env in run.sh $1 $2 $3"
+cd bahmni-gauge-amman/ && mvn gauge:execute -Denv=$1 -Dtags="$2" -DinParallel=$3
